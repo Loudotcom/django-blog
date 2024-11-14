5fab9536e5526d72662980fe3389ae774c51d597
Louise Anderson <ddq4yosl@students.codeinstitute.net>
2024-11-14 16:20:53 +0000

    add posts to homepage

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-loudotcom-djangoblog-gp8y5o0x2d8.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
