74691bfde68e7c697e7d7623c6dc6defa4bb416d
Louise Anderson <ddq4yosl@students.codeinstitute.net>
2024-11-21 12:41:24 +0000

    tidy up final code

--- about/views.py
+++ about/views.py
@@ -5,13 +5,25 @@ from .forms import CollaborateForm
 
 
 def about_me(request):
+    """
+    Renders the most recent information on the website author
+    and allows user collaboration requests
+    Displays an individual instance of :model:`about.About`.
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+    ``collacorate_form``
+        An instance of :model:`about.CollaborateForm`.
+    **Template:**
+    :template:`about/about.html`    
+    """
 
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
-            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
-
+            messages.add_message(request, messages.SUCCESS, 
+            "Collaboration request received! I endeavour to respond within 2 working days.")
 
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
@@ -19,7 +31,7 @@ def about_me(request):
     return render(
         request,
         "about/about.html",
-        {"about": about,
-        "collaborate_form": collaborate_form
-        }
+            {"about": about,
+             "collaborate_form": collaborate_form
+            }
     )
